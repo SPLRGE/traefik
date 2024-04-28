6d4fa3f76f1e10f9bdb745bcd588281b08e91d15
Victor <victor@splrge.dev>
2024-04-28 15:04:47 +0000

    (WIP) feat: implement auth

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,26 +1,12 @@
 import { DateTime } from 'luxon'
-import hash from '@adonisjs/core/services/hash'
-import { compose } from '@adonisjs/core/helpers'
 import { BaseModel, column } from '@adonisjs/lucid/orm'
-import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 
-const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
-  uids: ['email'],
-  passwordColumnName: 'password',
-})
-
-export default class User extends compose(BaseModel, AuthFinder) {
+export default class User extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
   @column()
-  declare fullName: string | null
-
-  @column()
-  declare email: string
-
-  @column()
-  declare password: string
+  declare ssoId: string // Reference microsoft entra id
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
