fb272e8e5c68e713ae61196a3b9146a34ff79c0e
Victor <victor@splrge.dev>
2024-06-21 14:55:42 +0000

    feat: add passwords on user accounts

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,16 +1,30 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, beforeSave, column } from '@adonisjs/lucid/orm'
+import hash from '@adonisjs/core/services/hash'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
   @column()
-  declare ssoId: string // Reference microsoft entra id
+  declare email: string
+
+  @column()
+  declare password: string | null
+
+  @column()
+  declare ssoId: string | null
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null
+
+  @beforeSave()
+  static async hashPassword(user: User) {
+    if (user.$dirty.password && user.password) {
+      user.password = await hash.make(user.password)
+    }
+  }
 }
--- database/migrations/1714181841018_create_users_table.ts
+++ database/migrations/1714181841018_create_users_table.ts
@@ -6,7 +6,10 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').notNullable()
-      table.string('sso_id').notNullable()
+
+      table.string('email').notNullable().unique()
+      table.string('password').nullable()
+      table.string('sso_id').nullable()
 
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
