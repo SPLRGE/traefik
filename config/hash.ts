89c30ff0b3e74426d86c10157df1270fea5848fc
Victor <victor@splrge.dev>
2024-06-21 14:55:42 +0000

    feat: add passwords on user accounts

--- config/hash.ts
+++ config/hash.ts
@@ -1,7 +1,7 @@
 import { defineConfig, drivers } from '@adonisjs/core/hash'
 
 const hashConfig = defineConfig({
-  default: 'scrypt',
+  default: 'argon',
 
   list: {
     scrypt: drivers.scrypt({
@@ -10,6 +10,15 @@ const hashConfig = defineConfig({
       parallelization: 1,
       maxMemory: 33554432,
     }),
+    argon: drivers.argon2({
+      version: 19,
+      variant: 'i',
+      iterations: 3,
+      memory: 65536,
+      parallelism: 4,
+      saltSize: 16,
+      hashLength: 32
+    })
   },
 })
 
