80461d757cb4c4f100045645b9f99efb6c5fc50f
Victor <victor@splrge.dev>
2024-06-22 00:14:18 +0000

    feat: hide microsoft login option if no client secret is provided

--- start/edge.ts
+++ start/edge.ts
@@ -1,4 +1,8 @@
 import edge from "edge.js";
 import {DateTime} from "luxon";
+import env from "#start/env";
 
 edge.global('luxon', DateTime)
+edge.global('microsoft', {
+    enabled: env.get('MICROSOFT_CLIENT_ID') && env.get('MICROSOFT_CLIENT_SECRET'),
+})
