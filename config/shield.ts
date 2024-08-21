d7a94b3c1371435d47c4a307dc5a8daefb6a95a8
Victor <victor@splrge.dev>
2024-08-21 13:17:46 +0000

    fix: disable csrf on setup route

--- config/shield.ts
+++ config/shield.ts
@@ -17,7 +17,7 @@ const shieldConfig = defineConfig({
    */
   csrf: {
     enabled: true,
-    exceptRoutes: [],
+    exceptRoutes: ['auth.setup'],
     enableXsrfCookie: false,
     methods: ['POST', 'PUT', 'PATCH', 'DELETE'],
   },
